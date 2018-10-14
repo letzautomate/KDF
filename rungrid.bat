5ea41723dd2cb3e60a56c593b2bedce333d33c95
krishna <letzautomate@gmail.com>
2018-10-14 08:52:46 +0530

    Remote Execution capabilities are added

---
+++ rungrid.bat
@@ -0,0 +1,2 @@
+start java -jar selenium-server-standalone-3.14.0.jar -role hub
+start java -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-standalone-3.14.0.jar -role webdriver -hub http://localhost:4444/grid/register -port 5555
